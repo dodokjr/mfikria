6c268bd36621ffaa549c843a9d65458c7d49777d
Dodokjr <ffikri604@gmail.com>
2023-09-02 11:44:48 +0700

    ssssss

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import DodokStore from "../../Assets/Projects/DodokStore.png";
 import Gabut from "../../Assets/Projects/gabut.png";
 import Myanimelist from "../../Assets/Projects/myanimelist.png";
+import ApiMfikria from "../../Assets/Projects/api.png";
 import GithubApi from "./GithubApi/GithubApi";
 
 
@@ -54,7 +55,17 @@ function Projects()
                 title="Search anime with api"
                 description="Project with frontend Reactjs and backend Expressjs api with jikan anime"
                 ghLink="https://github.com/dodokjr/animeList"
-                demoLink="https://myanimeelist.vercel.app/"
+                demoLink="https://myandlist.vercel.app/"
+              />
+            </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={ApiMfikria}
+                isBlog={false}
+                title="official Api By me"
+                description="Project with Backend Express js"
+                ghLink="https://github.com/dodokjr/api-mfikria"
+                demoLink="https://api-mfikria.vercel.app/"
               />
             </Col>
           </Row>
